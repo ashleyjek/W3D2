01157c0331a9eb52272aad787fc0b74108b353a4
Juan A <avilajajuan@gmail.com>
2023-05-16 14:24:34 -0400

    built board

---
+++ Memory Game/board.rb
@@ -0,0 +1,102 @@
+class Board 
+
+
+    def initialize
+
+        @board = Array.new(4) {Array.new(4,"?")}
+
+        # @populate = Card.new
+
+    end 
+
+    def [](pos)
+
+        row, col = pos 
+
+        @board[row][col]
+
+    end
+
+
+    def []=(pos, val)
+
+        row, col = pos 
+
+        @board[row][col] = val
+
+    end
+
+
+    def num_hidden
+
+        @board.flatten.count {|ele| ele.hide == false}
+
+    end 
+
+    def won?
+
+        @board.each do |row|
+            row.all? {|ele| ele == true }
+            return true 
+        end 
+
+    return false 
+
+    end 
+
+
+    def render 
+
+        @board.each do |row|
+            puts row.join(" ")
+        end 
+
+    end 
+
+
+    def place_cards 
+
+        while num_hidden < 16
+            r_row = rand(0...@board.length)
+            r_col = rand(0...@board.length)
+            value = ('a'..'h').to_a.sample
+            pos = [r_row, r_col]
+            self[pos] = Card.new(value)
+        end 
+
+    end 
+
+
+    def hidden_cards_grid
+
+
+        @board.map do |row|
+            row.map do |ele|
+                if ele != '?'
+                    "?"
+                end 
+            end 
+        end 
+
+
+    end
+
+
+    def self.print_board(arr)
+
+        arr.each do |row|
+            puts row.join(" ")
+        end 
+
+    end 
+
+
+    def cheat 
+
+        Board.print_board(@board)
+
+    end
+
+    
+
+end
