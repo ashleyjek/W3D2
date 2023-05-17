6c8736a4891fe641c6f3ac6d3417315e71ecff66
ashley <ashleykimca@gmail.com>
2023-05-16 20:15:09 -0400

    end of class

--- Memory Game/board.rb
+++ Memory Game/board.rb
@@ -64,16 +64,17 @@ class Board
 
 
     def hidden_board 
-
-        @board.map do |row|
-            row.map do |ele|
-                if ele != '?'
-                    '?'
-                end 
+        hidden = []
+        @board.each do |row|
+            subarr = []
+            row.each do |ele|
+                subarr << "?"
             end 
+            hidden << subarr
         end  
+        hidden
 
-
+        
     end 
 
 
@@ -89,9 +90,23 @@ class Board
     def reveal(pos)
         if self[pos].current_state == self[pos].hidden? 
             self[pos].switch_side
+            p hidden_board[0][0]
+            p face_value_board[0][0]
+            hidden_board[pos[0]][pos[1]] = face_value_board[pos[0]][pos[1]]
             return self[pos].face_val
         end 
     
+    end
+
+    def won?
+        @board.each do |row|
+            row.each do |card|
+                if card.hidden?
+                    return false
+                end
+            end
+        end
+        true
     end
 
 
